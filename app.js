0487828f74440fbd8d0442676c2dfdbbcef234a7
Julian Hollmann <julian.hollmann@googlemail.com>
2012-11-11 17:43:54 +0100

    Express init

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-var express = require('express');
-var config = require('./config');
-var app = express();
+/**
+ * Module dependencies.
+ */
 
-app.listen(3000);
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , config = require('./config')
+  , mysql = require('mysql')
+  , path = require('path');
+
+var app = express();
+var connection  = mysql.createConnection({
+    host     : config.db_host,
+    user     : config.db_username,
+    password : config.db_password
+});
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
